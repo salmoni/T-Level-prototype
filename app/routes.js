bd6af0967cde3bdc6d236aaca15cbae002151269
Alan James Salmoni <alan@thoughtintodesign.com>
2019-08-30 17:52:45 +0100

    Sets whether user is admin or not when signing in

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,12 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.post('/1-0/AO/action-signin', function (req, res) {
+    if (req.session.data['Signin-username'] === 'admin') {
+        req.session.data['staff-role'] = 'admin'
+    } else {
+        req.session.data['staff-role'] = 'staff'
+    }
+    res.redirect('/1-0/AO/hub')
+})
