703beed516ecce6069adf5b269041d5351ee6b90
Alan James Salmoni <alan@thoughtintodesign.com>
2019-09-02 09:16:32 +0100

    Added various functions...
    First go (plain prototype rather than properly interactive) to preview bulk upload, upload providers in bulk, and upload providers singly.

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,10 @@ const router = express.Router()
 module.exports = router
 
 router.post('/1-0/AO/action-signin', function (req, res) {
+    /* 
+    If user signs in with "admin" as user name, they get administrative rights and accesses. 
+    Else they do not. 
+    */
     if (req.session.data['Signin-username'] === 'admin') {
         req.session.data['staff-role'] = 'admin'
     } else {
@@ -14,3 +18,24 @@ router.post('/1-0/AO/action-signin', function (req, res) {
     res.redirect('/1-0/AO/hub')
 })
 
+router.post('/1-0/AO/upload-provider-bulk', function (req, res) {
+    /* 
+    Takes a form uploaded by the user and converts them into new providers.
+    */
+
+    res.redirect('/1-0/AO/preview-bulk-records')
+})
+
+router.post('/1-0/AO/action-import-providers-bulk', function (req, res) {
+    /*
+    Takes the bulk-imported records from user preview and brings them into the main catalogue.
+    */
+    res.redirect('/1-0/AO/hub')
+})
+
+router.post('/1-0/AO/action-import-providers-single', function (req, res) {
+    /*
+    Takes the single entered record from user preview and brings it into the main catalogue.
+    */
+    res.redirect('/1-0/AO/hub')
+})
