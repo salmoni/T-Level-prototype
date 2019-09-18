2b306fda5bcf96c84def18b314b004670ba7d70d
Alan James Salmoni <alan@thoughtintodesign.com>
2019-09-18 14:47:37 +0100

    Changed serviceName to match service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Results and Certification service',
+  serviceName: 'T Levels management',
 
   // Default port that prototype runs on
   port: '3000',
