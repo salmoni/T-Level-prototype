c72f432e49504b825777863a5561e67f52c14b70
Alan James Salmoni <alan@thoughtintodesign.com>
2019-09-19 17:53:58 +0100

    Revert "Changed serviceName to match service name"
    This reverts commit 2b306fda5bcf96c84def18b314b004670ba7d70d.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'T Levels management',
+  serviceName: 'Results and Certification service',
 
   // Default port that prototype runs on
   port: '3000',
