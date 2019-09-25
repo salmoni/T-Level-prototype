57041fb7b6f1fe8cb8b3d550c71cce1b69413516
Alan James Salmoni <alan@thoughtintodesign.com>
2019-09-25 13:08:46 +0100

    Change service name
    To something more service-like

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Results and Certification service',
+  serviceName: 'Manage T Levels',
 
   // Default port that prototype runs on
   port: '3000',
