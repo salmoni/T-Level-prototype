0281b6cbd433c43f00260199ea9314c8006a6484
Alan James Salmoni <alan@thoughtintodesign.com>
2019-09-25 10:08:00 +0100

    Only importing what routes modules are necessary

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/1-0/AO/sign-in', function (req, res) {
+    require('./routes/routes-1-0.js')(router)
+    res.render('1-0/AO/sign-in')
+})
+
+router.get('/1-1/AO/sign-in', function (req, res) {
+    require('./routes/routes-1-1.js')(router)
+    res.render('1-1/AO/sign-in')
+})
 
 
 module.exports = router
-require('./routes/routes-1-0.js')(router)
-require('./routes/routes-1-1.js')(router)
 //checkIfActive(router.req)
