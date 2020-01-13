b49a84bb82786c7801a29b0f6fe3bf2ab353c581
Alan James Salmoni <alan@thoughtintodesign.com>
2020-01-13 15:56:19 +0000

    Minor content change
    "added" -> "linked"

--- app/routes/routes-1-5.js
+++ app/routes/routes-1-5.js
@@ -152,7 +152,7 @@ module.exports = function (router) {
         //req.session.data['providers'] = req.session.data['providers'].concat(req.session.data['providers-added'])
         //req.session.data['providers-tmp'] = req.session.data['providers']
 
-        conf = "You have added 4 new providers to T Levels"
+        conf = "You have linked 4 new providers to T Levels"
 
         req.session.data['errors'] = null
         req.session.data['add-ao-selected'] = null
